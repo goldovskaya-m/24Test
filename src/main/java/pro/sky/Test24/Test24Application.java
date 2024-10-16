fc381d23f903ced88a474e54af96cb234796d2b0
goldovskaya-m <ariya-aaa@yandex.ru>
2024-10-16 22:15:25 +0300

    Initial commit

---
+++ src/main/java/pro/sky/Test24/Test24Application.java
@@ -0,0 +1,13 @@
+package pro.sky.Test24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test24Application.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/Test24/Test24ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.Test24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
